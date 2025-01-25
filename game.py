7bce909f6be5d8ae3be775b34c71eb24b91482ea
VeigarNg <129383084+VeigarNg@users.noreply.github.com>
2025-01-25 21:37:13 +0800

    Update game.py

--- game.py
+++ game.py
@@ -100,9 +100,9 @@ ax.plot([0,0],[-1,1],color="black")
 ax.pie(st.session_state.puzzle.plotter,colors=pie,radius=1,center=(0,0))
 plt.tight_layout()
 fig.subplots_adjust(
-    top = 0.5,
+    top = 1,
     bottom = 0.5,
-    left = 0.25,
-    right = 0.5
+    left = 0.5,
+    right = 1
 )
 cont_plot.pyplot(fig,use_container_width=False)

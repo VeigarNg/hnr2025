73ca93a6a7beaecf7563c0cf2f3fbcff8ea1b2a7
VeigarNg <129383084+VeigarNg@users.noreply.github.com>
2025-01-25 20:37:08 +0800

    Update game.py

--- game.py
+++ game.py
@@ -68,7 +68,7 @@ if not st.session_state.initial:
 t = np.arange(0,2*np.pi+0.1,0.1)
 x = np.cos(t)
 y = np.sin(t)
-fig, ax = plt.subplots(figsize=(5.0,5.0))
+fig, ax = plt.subplots(figsize=(2.0,2.0))
 
 color = ["yellow","cyan","darkturquoise","gold"]
 pie = ["none"]*16
